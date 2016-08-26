ac7d467b21aea7c25ca6d9eb62ddc67f2d6d46b9
xajil <xajil.developer@gmail.com>
2016-08-26 16:25:09 -0600

    Inicio de Log Linux
    Inicio de comandos en Linux, Centos, Ubuntu, RHEL

---
+++ linux_log.bash
@@ -0,0 +1,15 @@
+# Linux/Centos
+# Log Real Aplication y la respectiva descripción de los pasos y comando utilizados.
+# Esto pretende ser un log personal.
+# Neftali Sacuj
+# 2016.
+
+
+#Comandos para determinar la Release instalada.
+# Versión de Centos / RHEL 
+cat /etc/redhat-release
+
+
+# Versión de Ubuntu
+cat /etc/lsb-release
+
---
+++ mysql_log.sql
@@ -0,0 +1,10 @@
+/* Mysql  
+# Linux/Centos
+# Log Real Aplication y la respectiva descripción de los pasos y comando utilizados.
+# Esto pretende ser un log personal.
+# Neftali Sacuj
+# 2016.
+*/
+
+mysql -u root -p 
+
